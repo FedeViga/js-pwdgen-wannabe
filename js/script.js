ce5f7b2caf1905509e8826d8998dea396481898d
federico_viganò <fede_viga@hotmail.it>
2024-01-25 16:40:26 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ let lastName = prompt("Type your Last Name");
 let favColor = prompt("Type your favourite color");
 let randomNumber = Math.floor((Math.random() * 100) + 1)
 
-document.getElementById("password").innerHTML = ` Your AI Generated Password is * : ${firstName}${lastName}${favColor}${randomNumber}` ;
+document.getElementById("password").innerHTML = `${firstName}${lastName}${favColor}${randomNumber}` ;
