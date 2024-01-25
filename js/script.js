66d09b61adc74d3b64431e1a91462a03c9a2d243
federico_viganò <fede_viga@hotmail.it>
2024-01-25 15:56:08 +0100

    creato generatore di password

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Type your First Name");
+let lastName = prompt("Type your Last Name");
+let favColor = prompt("Type your favourite color");
+let randomNumber = Math.floor((Math.random() * 100) + 1)
+
+document.getElementById("password").innerHTML = ` Your AI Generated Password is * : ${firstName}${lastName}${favColor}${randomNumber}` ;
